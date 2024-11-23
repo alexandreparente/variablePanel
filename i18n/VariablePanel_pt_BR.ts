28f46060eb9dd8a0264872ffa4bc1c307fcdcf69
alexandreparente <alexandre.parente@gmail.com>
2024-11-23 17:35:19 -0300

    Add files via upload
    Initial commit

---
+++ i18n/VariablePanel_pt_BR.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>VariablePanel</name>
+    <message>
+        <location filename="../variable_panel.py" line="165"/>
+        <source>&amp;Variable Panel</source>
+        <translation>&amp;Painel de variáveis</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="156"/>
+        <source>Variable Panel</source>
+        <translation>Painel de variáveis</translation>
+    </message>
+</context>
+<context>
+    <name>VariablePanelDockWidget</name>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="44"/>
+        <source>Variable Panel</source>
+        <translation>Painel de variáveis</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="59"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="60"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="61"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
---
+++ zip_build/variable_panel/i18n/VariablePanel_en.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>VariablePanel</name>
+    <message>
+        <location filename="../variable_panel.py" line="165"/>
+        <source>&amp;Variable Panel</source>
+        <translation>&amp;Variable Panel</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="156"/>
+        <source>Variable Panel</source>
+        <translation>Variable Panel</translation>
+    </message>
+</context>
+<context>
+    <name>VariablePanelDockWidget</name>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="44"/>
+        <source>Variable Panel</source>
+        <translation>Variable Panel</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="59"/>
+        <source>Apply</source>
+        <translation>Apply</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="60"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="61"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
