13d3c35293562a3a54a1cf8bbfacd619419bd73e
alexandreparente <alexandre.parente@gmail.com>
2025-01-13 08:27:26 -0300

    - Added Spanish language support

---
+++ i18n/VariablePanel_es.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>VariablePanel</name>
+    <message>
+        <location filename="../variable_panel.py" line="224"/>
+        <source>&amp;Variables Panel</source>
+        <translation>&amp;Panel de Variables</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="154"/>
+        <source>Variables Panel</source>
+        <translation>Panel de Variables</translation>
+    </message>
+</context>
+<context>
+    <name>VariablePanelDockWidget</name>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="58"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="59"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="60"/>
+        <source>OK</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="43"/>
+        <source>Variables Panel</source>
+        <translation>Panel de Variables</translation>
+    </message>
+</context>
+</TS>
