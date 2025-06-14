e69c62017e735422164ef0227f72106b8d1e0496
alexandreparente <alexandre.parente@gmail.com>
2025-06-14 16:21:09 -0300

    - Add German translation

---
+++ i18n/VariablePanel_de.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>VariablePanel</name>
+    <message>
+        <location filename="../variable_panel.py" line="222"/>
+        <source>&amp;Variables Panel</source>
+        <translation>&amp;Bedienfeld Variablen</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="153"/>
+        <source>Variables Panel</source>
+        <translation>Bedienfeld Variablen</translation>
+    </message>
+</context>
+<context>
+    <name>VariablePanelDockWidget</name>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="57"/>
+        <source>Apply</source>
+        <translation>Anwenden</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="58"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="59"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="42"/>
+        <source>Variables Panel</source>
+        <translation>Bedienfeld Variablen</translation>
+    </message>
+</context>
+</TS>
