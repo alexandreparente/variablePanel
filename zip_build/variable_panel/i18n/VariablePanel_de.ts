14e50170e07af7f0ebb15adc778926a6936136b9
alexandreparente <alexandre.parente@gmail.com>
2025-09-22 08:52:12 -0300

    -  Update version

---
+++ zip_build/variable_panel/i18n/VariablePanel_de.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>VariablePanel</name>
+    <message>
+        <location filename="../variable_panel.py" line="222"/>
+        <source>&amp;Variables Panel</source>
+        <translation>&amp;Bedienfeld Variablen</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="153"/>
+        <source>Variables Panel</source>
+        <translation>Bedienfeld Variablen</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="64"/>
+        <source>Variables ToolBar</source>
+        <translation>Variablenwerkzeugleiste</translation>
+    </message>
+</context>
+<context>
+    <name>VariablePanelDockWidget</name>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="46"/>
+        <source>Variables</source>
+        <translation>Variablen</translation>
+    </message>
+</context>
+</TS>
---
+++ zip_build/variable_panel/i18n/VariablePanel_es.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>VariablePanel</name>
+    <message>
+        <location filename="../variable_panel.py" line="222"/>
+        <source>&amp;Variables Panel</source>
+        <translation>&amp;Panel de Variables</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="153"/>
+        <source>Variables Panel</source>
+        <translation>Panel de Variables</translation>
+    </message>
+    <message>
+        <location filename="../variable_panel.py" line="64"/>
+        <source>Variables ToolBar</source>
+        <translation>Barra de herramientas de variables</translation>
+    </message>
+</context>
+<context>
+    <name>VariablePanelDockWidget</name>
+    <message>
+        <location filename="../variable_panel_dockwidget.py" line="46"/>
+        <source>Variables</source>
+        <translation>Variables</translation>
+    </message>
+</context>
+</TS>
